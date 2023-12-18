fc83ea6e777d5e4bdf96b7468dea019477d24cd0
sumit <ben.sumit@gmail.com>
2023-12-18 16:42:37 +0530

    python_Bot_BS4
    python bot for scrapping IMDB top 250 movies using BeautifulSoup

--- IMDB_BOT_BS4.py
+++ IMDB_BOT_BS4.py
@@ -9,6 +9,22 @@ response.raise_for_status()
 html = response.text 
 try:
     data = BeautifulSoup(html,"html.parser")
-    print(data)
+    main = data.find("ul",class_="ipc-metadata-list ipc-metadata-list--dividers-between sc-71ed9118-0 kxsUNk compact-list-view ipc-metadata-list--base")
+    titles = main.find_all("li") # type: ignore
+    with open("Movie_list_using_bs4.txt","w") as f:
+        headers ="Data \n"
+        for title in titles:
+            name = title.find('div',class_="ipc-title ipc-title--base ipc-title--title ipc-title-link-no-icon ipc-title--on-textPrimary sc-43986a27-9 gaoUku cli-title").h3.get_text()
+            year = title.find('div',class_="sc-43986a27-7 dBkaPT cli-title-metadata").span.get_text()
+            duration = title.find_all('span',class_="sc-43986a27-8 jHYIIK cli-title-metadata-item")[1].get_text()
+            link = title.find('div',class_="ipc-title ipc-title--base ipc-title--title ipc-title-link-no-icon ipc-title--on-textPrimary sc-43986a27-9 gaoUku cli-title").a["href"]
+            f.write(name+'\n')
+            f.write(f'Year Released : {year}'+'\n')
+            f.write(f'Duration:{duration}'+'\n')
+            f.write(f'Link: {url+link}'+'\n')
+            f.write("--------------------------------------"+'\n')
+    
+
+    f.close()
 except Exception as e:
     print(e)
